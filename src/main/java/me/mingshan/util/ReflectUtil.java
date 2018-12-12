091de42022c3d1a83315c4eed4ba67f3bb43d49a
Juntao Han <499445428@qq.com>
2018-12-12 23:23:53 +0800

    Update ReflectUtil.java

--- src/main/java/me/mingshan/util/ReflectUtil.java
+++ src/main/java/me/mingshan/util/ReflectUtil.java
@@ -158,6 +158,7 @@ public class ReflectUtil {
     }
 
     public static Field[] getAccessibleField() {
+        List<Field> fieldList = new ArrayList<>();
         return null;
     }
 
@@ -208,5 +209,32 @@ public class ReflectUtil {
         } else {
             return "default";
         }
+    }
+
+    private Class<?> boxing(final Class<?> type) {
+        if (type == null) {
+            return null;
+        } else if (type.isPrimitive()) {
+            if (boolean.class == type) {
+                return Boolean.class;
+            } else if (int.class == type) {
+                return Integer.class;
+            } else if (long.class == type) {
+                return Long.class;
+            } else if (short.class == type) {
+                return Short.class;
+            } else if (byte.class == type) {
+                return Byte.class;
+            } else if (double.class == type) {
+                return Double.class;
+            } else if (float.class == type) {
+                return Float.class;
+            } else if (char.class == type) {
+                return Character.class;
+            } else if (void.class == type) {
+                return Void.class;
+            }
+        }
+        return type;
     }
 }

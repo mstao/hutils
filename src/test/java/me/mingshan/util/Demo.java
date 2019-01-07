eda143676cf803816527f209461ed561990c126e
hanjuntao <hanjuntao@hd123.com>
2019-01-07 15:27:40 +0800

    Add caffeine cache.

---
+++ src/test/java/me/mingshan/util/Demo.java
@@ -0,0 +1,20 @@
+package me.mingshan.util;
+
+public class Demo {
+    private int tryBlock;
+    private int catchBlock;
+    private int finallyBlock;
+    private int methodExit;
+
+    public void test() {
+        try {
+            tryBlock = 0;
+        } catch (Exception e) {
+            catchBlock = 1;
+        } finally {
+            finallyBlock = 2;
+        }
+        methodExit = 3;
+    }
+}
+

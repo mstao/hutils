d343d927dd9ffbd80b05a258e35643c1cb7a7ac4
Juntao Han <walkerhan@126.com>
2022-01-07 12:40:21 +0800

    Update TimeUtil.java

--- src/main/java/me/mingshan/util/TimeUtil.java
+++ src/main/java/me/mingshan/util/TimeUtil.java
@@ -61,7 +61,7 @@ public class TimeUtil {
     /**
      * Formats a date-time into a date/time string by default format.
      *
-     * @param time the time value to be formatted into a time string
+     * @param time   the time value to be formatted into a time string
      * @param format the specified format
      * @return the formatted string
      */
@@ -70,11 +70,24 @@ public class TimeUtil {
     }
 
     /**
+     * 格式化日期
+     *
+     * @param date   日期
+     * @param format 格式
+     * @return 日期字符串
+     */
+    public static String format(Date date, TimeFormat format) {
+        LocalDateTime localDateTime = date2LocalDateTime(date);
+
+        return format(localDateTime, format);
+    }
+
+    /**
      * Obtains an instance of {@code LocalDateTime} from a text string using a specific formatter.
      * <p>
      * The text is parsed using the formatter, returning a date-time.
      *
-     * @param text  the text to parse, not null
+     * @param text the text to parse, not null
      * @return the parsed local date-time, not null
      */
     public static LocalDateTime parse(String text) {
@@ -86,12 +99,24 @@ public class TimeUtil {
      * <p>
      * The text is parsed using the formatter, returning a date-time.
      *
-     * @param text  the text to parse, not null
-     * @param format  the formatter to use, not null
+     * @param text   the text to parse, not null
+     * @param format the formatter to use, not null
      * @return the parsed local date-time, not null
      */
     public static LocalDateTime parse(String text, TimeFormat format) {
         return LocalDateTime.parse(text, format.formatter);
+    }
+
+    /**
+     * 解析时间
+     *
+     * @param text   时间字符串
+     * @param format 格式化
+     * @return 时间
+     */
+    public static Date parseDate(String text, TimeFormat format) {
+        LocalDateTime parse = parse(text, format);
+        return localDateTime2Date(parse);
     }
 
     /**
@@ -133,24 +158,25 @@ public class TimeUtil {
      * 日期相隔天数
      *
      * @param startDateInclusive 开始时间
-     * @param endDateExclusive 结束时间
+     * @param endDateExclusive   结束时间
      * @return 天数
      */
     public static int periodDays(LocalDate startDateInclusive, LocalDate endDateExclusive) {
-        return Period.between(startDateInclusive, endDateExclusive).getDays();
+        return (int) (endDateExclusive.toEpochDay() - startDateInclusive.toEpochDay());
     }
 
     /**
      * 日期相隔天数
      *
      * @param startDateInclusive 开始时间
-     * @param endDateExclusive 结束时间
+     * @param endDateExclusive   结束时间
      * @return 天数
      */
     public static int periodDays(Date startDateInclusive, Date endDateExclusive) {
         LocalDate startLocalDate = date2LocalDate(startDateInclusive);
         LocalDate endLocalDate = date2LocalDate(endDateExclusive);
-        return Period.between(startLocalDate, endLocalDate).getDays();
+
+        return (int) (endLocalDate.toEpochDay() - startLocalDate.toEpochDay());
     }
 
     /**
@@ -173,6 +199,50 @@ public class TimeUtil {
     public static boolean isAfterToday(Date date) {
         LocalDate now = LocalDate.now();
         return date2LocalDate(date).isAfter(now);
+    }
+
+    public static Date localDate2Date(LocalDate localDate) {
+        ZoneId zone = ZoneId.systemDefault();
+        Instant instant = localDate.atStartOfDay().atZone(zone).toInstant();
+        return Date.from(instant);
+    }
+
+    /**
+     * 获取当前的前n天时间
+     *
+     * @param beforeDays 前n天
+     * @return 当前的前n天时间
+     */
+    public static Date beforeDay(int beforeDays) {
+        LocalDate now = LocalDate.now();
+        LocalDate localDate = now.minusDays(beforeDays);
+        return localDate2Date(localDate);
+    }
+
+    /**
+     * 获取传入日期的起始时间，比如传入2021-08-09 12:00:00
+     * <p>
+     * 返回值为 2021-08-09 00:00:00
+     *
+     * @param date 传入日期
+     * @return 传入日期的起始时间
+     */
+    public static Date dayOfStartTime(Date date) {
+        LocalDateTime beginTime = LocalDateTime.of(date2LocalDate(date), LocalTime.MIN);
+        return localDateTime2Date(beginTime);
+    }
+
+    /**
+     * 获取传入日期的结束时间，比如传入2021-08-09 12:00:00
+     * <p>
+     * 返回值为 2021-08-09 23:59:59
+     *
+     * @param date 传入日期
+     * @return 传入日期的结束时间
+     */
+    public static Date dayOfEndTime(Date date) {
+        LocalDateTime endTime = LocalDateTime.of(date2LocalDate(date), LocalTime.MAX);
+        return localDateTime2Date(endTime);
     }
 
     /**
@@ -221,10 +291,15 @@ public class TimeUtil {
         LONG_DATE_PATTERN_WITH_MILSEC_NONE_24("yyyyMMdd HH:mm:ss.SSS");
 
         private transient DateTimeFormatter formatter;
+        private String pattern;
 
         TimeFormat(String pattern) {
             this.formatter = DateTimeFormatter.ofPattern(pattern);
+            this.pattern = pattern;
         }
 
+        public String getPattern() {
+            return pattern;
+        }
     }
 }
